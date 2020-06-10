666b19b1d1bc15b5ef427d1fc0cb25133c682a44
unknown <kirit.jbs@gmail.com>
2020-06-10 19:48:43 +0545

    Payment design Changes

--- app/containers/Checkout/styles.js
+++ app/containers/Checkout/styles.js
@@ -13,6 +13,50 @@ export default {
   marginRight:Layout.indent,
   fontFamily:'Font-Medium',
   },
+  payOptions:{
+    fontSize:12,
+    fontFamily:'Font-Medium',
+    lineHeight:18
+  },
+  payOptionscard:{
+    fontSize:14,
+    fontFamily:'Font-Medium',
+    lineHeight:18,
+
+  },
+PayMethod:{
+  borderColor:Colors.primary,
+  marginRight:Layout.indent,
+  borderBottomWidth:1.2,
+  marginBottom:5,
+  paddingTop:10,
+  paddingBottom:15,
+  height:50
+  
+},
+payRadio:{
+  width:50,
+  marginLeft:Layout.indent
+},
+PayMethodOther:{
+    marginRight:Layout.indent,
+   marginBottom:0,
+  paddingBottom:10,
+  height:60
+},
+walletBtn:{
+  backgroundColor:Colors.secondary,
+  paddingVertical:10,
+  borderRadius:7,
+  minWidth:100
+
+},
+walletBtnText:{
+color:Colors.white,
+fontSize:13,
+paddingHorizontal:7,
+fontFamily:'Font-Medium'
+},
 ItemList:{
   borderColor:Colors.primary,
   marginRight:Layout.indent,
