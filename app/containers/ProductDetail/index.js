4e3e847af2c7061b59dbf851ad3b3270f05947c3
Bhagirihi <tatvmasiproject@gmail.com>
2020-12-16 22:57:51 +0530

    VEG ICON

--- app/containers/ProductDetail/index.js
+++ app/containers/ProductDetail/index.js
@@ -319,7 +319,7 @@ class ProductDetail extends React.Component {
                           </View>
                           <View style={{ flex: 0, width: 12 }}>
                             {
-                              item.foodType != "NA" || item.foodType != null  && <Image style={{ width: 12, height: 12 }} source={ProductDetail.item[0].foodType == 'veg' ? imgs.smallVeg
+                              (item.foodType != "NA" || item.foodType != null)  && <Image style={{ width: 12, height: 12 }} source={ProductDetail.item[0].foodType == 'veg' ? imgs.smallVeg
                                 :
                                 ProductDetail.item[0].foodType == 'vegan' ? imgs.smallVegan
                                   :
